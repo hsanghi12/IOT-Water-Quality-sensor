6ec43c9900338ca2b0a24960a94ff047591e97b5
hsanghi12 <82219638+hsanghi12@users.noreply.github.com>
2025-05-16 16:03:32 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,2 +1,3 @@
 import streamlit as st
-st.title("This is my homepage")
+st.title("AquaSentinel: AI-Powered IoT Guardians Ensuring Rural Water Purity")
+st.write("This system leverages cost-effective IoT sensors to continuously capture pH, turbidity, dissolved oxygen, conductivity, and TDS data from rural water sources. A trained machine learning model analyzes these parameters in automated real time to predict water quality, detect anomalies, and trigger alerts—enabling proactive management and ensuring safe drinking water.")
