16063cfc9a5764fcbb781364ab15fc73767fe457
hsanghi12 <82219638+hsanghi12@users.noreply.github.com>
2025-05-24 14:21:57 +0530

    Update About.py

--- About.py
+++ About.py
@@ -1,3 +1,8 @@
 import streamlit as st
 def app():
-  st.write("""I am Harshit""")
+  st.markdown("***Harshit Sanghi***, a *16-year-old* student from *Step by Step School, Noida*, isn’t just tinkering with tech—he’s using it to **save lives**.  
+
+Moved by the unsafe water conditions in rural India, he developed **AquaSentinel**, an *IoT-powered* system that monitors water quality in real time.  
+
+Driven by *purpose*, *curiosity*, and **दिल से देश के लिए कुछ करने का जुनून**, Harshit is proving that **innovation has no age**.
+")
